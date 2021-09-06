723887a8bc4ec0b71feb4b04cbee2a437e36c3d6
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-06 17:05:23 +0900

    :gear: :: 초기 세팅

---
+++ src/main/java/com/projectflow/projectflowwebsocket/ProjectFlowWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.projectflow.projectflowwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFlowWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFlowWebsocketApplication.class, args);
+    }
+
+}
