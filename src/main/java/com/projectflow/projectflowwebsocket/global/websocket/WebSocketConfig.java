38e197bfbf68269b684fd0f82c414bbced9ab824
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-07 11:54:54 +0900

    ⚡️ :: (#2) Webflux 버림

--- src/main/java/com/projectflow/projectflowwebsocket/global/websocket/WebSocketConfig.java
+++ src/main/java/com/projectflow/projectflowwebsocket/global/websocket/WebSocketConfig.java
@@ -1,33 +1,44 @@
 package com.projectflow.projectflowwebsocket.global.websocket;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.HandlerAdapter;
-import org.springframework.web.reactive.HandlerMapping;
-import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
-import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
-
-import java.util.Map;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    public static final String ENDPOINT = "/socket";
+    @Value("${rabbit.url}")
+    private String host;
 
-    @Bean
-    public HandlerAdapter handlerAdapter() {            // global websocket 설정함. Spring Webflux 에서 websocket 을 킴
-        return new WebSocketHandlerAdapter();
+    @Value("${rabbit.password}")
+    private String password;
+
+    @Value("${rabbit.port}")
+    private int port;
+
+    @Value("${rabbit.username}")
+    private String username;
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").withSockJS();
     }
 
-    @Bean
-    public HandlerMapping handlerMapping(WebSocketHandler webSocketHandler) {            // Spring 이 Socket 연결을 시작하기 위한 endpoint 설정
-
-        Map<String, WebSocketHandler> map = Map.of(ENDPOINT, webSocketHandler);
-
-        return new SimpleUrlHandlerMapping(map, -1);            // -1 이라는 우선순위로 등록
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+//        registry.enableSimpleBroker("/topic");      // in memory topic 을 사용.
+        registry.enableStompBrokerRelay("/topic")       // 외부 MQ 사용 설정
+                .setRelayHost(host)
+                .setRelayPort(port)
+                .setClientLogin(username)
+                .setClientPasscode(password);
     }
 
 }
