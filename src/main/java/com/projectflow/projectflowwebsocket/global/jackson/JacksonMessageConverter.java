79b00791236d67d7dff70a92ec247486e4224b07
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-08 20:16:25 +0900

    ⚡️ :: (#2) Jackson 설정 추가

--- src/main/java/com/projectflow/projectflowwebsocket/global/jackson/JacksonMessageConverter.java
+++ src/main/java/com/projectflow/projectflowwebsocket/global/jackson/JacksonMessageConverter.java
@@ -1,2 +1,21 @@
-package com.projectflow.projectflowwebsocket.global.jackson;public class JacksonMessageConverter {
+package com.projectflow.projectflowwebsocket.global.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonMessageConverter {
+
+    @Bean
+    Jackson2JsonMessageConverter jsonMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);       // Message 에서 DATETIME 파싱을 위해 사용
+        objectMapper.registerModule(new JavaTimeModule());
+
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
 }
