4daa4f07e93ad364d199df0f478548d5c95210bb
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-09 12:37:32 +0900

    ⚡️ :: .env 파일 포기

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,8 @@
 sudo docker pull jhhong0509/project-flow:latest
-sudo docker run -d --env-file /home/ubuntu/.env -p 8080:8080 --name project-flow jhhong0509/project-flow
+sudo docker run -d -p 8080:8080 --name project-flow jhhong0509/project-flow \
+    -e MONGO_URL "$MONGO_URL" \
+    -e SECRET_KEY "$SECRET_KEY" \
+    -e RABBIT_PORT "$RABBIT_PORT" \
+    -e RABBIT_USERNAME "$RABBIT_USERNAME" \
+    -e RABBIT_PASSWORD "$RABBIT_PASSWORD" \
+    -e RABBIT_HOST "$RABBIT_HOST"
