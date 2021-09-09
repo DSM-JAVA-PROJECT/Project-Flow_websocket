14a1761f011a1c54f2aaea3666dd44b30c80f9c9
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-09 12:46:34 +0900

    ⚡️ :: 버그 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,9 @@
 sudo docker pull jhhong0509/project-flow:latest
 sudo docker run -d -p 8080:8080 --name project-flow \
-    -e MONGO_URL "$MONGO_URL" \
-    -e SECRET_KEY "$SECRET_KEY" \
-    -e RABBIT_PORT "$RABBIT_PORT" \
-    -e RABBIT_USERNAME "$RABBIT_USERNAME" \
-    -e RABBIT_PASSWORD "$RABBIT_PASSWORD" \
-    -e RABBIT_HOST "$RABBIT_HOST" \
+    -e MONGO_URL="$MONGO_URL" \
+    -e SECRET_KEY="$SECRET_KEY" \
+    -e RABBIT_PORT="$RABBIT_PORT" \
+    -e RABBIT_USERNAME="$RABBIT_USERNAME" \
+    -e RABBIT_PASSWORD="$RABBIT_PASSWORD" \
+    -e RABBIT_HOST="$RABBIT_HOST" \
     jhhong0509/project-flow
