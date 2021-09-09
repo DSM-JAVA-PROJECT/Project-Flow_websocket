f79f8e9024988fc9e713d6931d6d77fe0c372b6a
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-09 17:10:53 +0900

    ⚡️ :: 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,3 @@
 sudo docker pull jhhong0509/project-flow:latest
 sudo -s source /home/ubuntu/.bashrc
-sudo docker run -d -p 8080:8080 --name project-flow \
-    -e MONGO_URL="$MONGO_URL" \
-    -e SECRET_KEY="$SECRET_KEY" \
-    -e RABBIT_PORT="$RABBIT_PORT" \
-    -e RABBIT_USERNAME="$RABBIT_USERNAME" \
-    -e RABBIT_PASSWORD="$RABBIT_PASSWORD" \
-    -e RABBIT_HOST="$RABBIT_HOST" \
-    jhhong0509/project-flow
+sudo docker run -d -p 8080:8080 --name project-flow jhhong0509/project-flow
