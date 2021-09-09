2c8262c82ea89248f258643116b0ef36f0d26577
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-09 10:52:52 +0900

    ⚡️ :: docker에 권한 부여

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
-docker pull project-flow-image
-docker run --env-file /home/ubuntu/.env -p 8080:8080 --name project-flow project-flow-image
+sudo docker pull project-flow-image
+sudo docker run --env-file /home/ubuntu/.env -p 8080:8080 --name project-flow project-flow-image
