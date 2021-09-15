2c444e2841945e3de81870f46dec7980311d1334
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-15 20:05:04 +0900

    ⚡️ :: (#4) 채팅방 컨트롤러 수정

--- src/main/java/com/projectflow/projectflowwebsocket/domain/chatroom/controller/ChatRoomController.java
+++ src/main/java/com/projectflow/projectflowwebsocket/domain/chatroom/controller/ChatRoomController.java
@@ -1,7 +1,9 @@
 package com.projectflow.projectflowwebsocket.domain.chatroom.controller;
 
+import com.projectflow.projectflowwebsocket.domain.chatroom.entity.ChatRoom;
 import com.projectflow.projectflowwebsocket.domain.chatroom.payload.CreateChatRoomRequest;
 import com.projectflow.projectflowwebsocket.domain.chatroom.service.ChatRoomService;
+import com.projectflow.projectflowwebsocket.domain.message.service.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -14,16 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatRoomController {
 
     private final ChatRoomService chatRoomService;
+    private final MessageService messageService;
 
     @MessageMapping("/create/chatroom/{projectId}")
     public int createChatRoom(@DestinationVariable String projectId,
                               @Payload CreateChatRoomRequest request) {
-        return chatRoomService.createChatRoom(projectId, request);
+        ChatRoom chatRoom = chatRoomService.createChatRoom(projectId, request);
+        messageService.sendCreateChatRoomMessage(projectId, chatRoom);
+        return 201;
     }
 
     @MessageMapping("/join/chatroom/{chatRoomId}")
-    public int createChatRoom(@DestinationVariable String chatRoomId) {
-        return chatRoomService.joinChatRoom(chatRoomId);
+    public int joinChatRoom(@DestinationVariable String chatRoomId) {
+        String roomId = chatRoomService.joinChatRoom(chatRoomId);
+        messageService.sendJoinMessage(roomId);
+        return 200;
     }
 
     @MessageMapping("/list/chatroom/{projectId}")
