ba7d75d4c2558c9f7a63e5a362cbe328a0fb12cf
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-18 12:27:38 +0900

    ⚡️ :: (#4) Repository 수정

--- src/main/java/com/projectflow/projectflowwebsocket/domain/chatroom/entity/ChatRoomRepository.java
+++ src/main/java/com/projectflow/projectflowwebsocket/domain/chatroom/entity/ChatRoomRepository.java
@@ -1,8 +1,10 @@
 package com.projectflow.projectflowwebsocket.domain.chatroom.entity;
 
+import com.projectflow.projectflowwebsocket.domain.user.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRoomRepository extends CustomChatRoomRepository, MongoRepository<ChatRoom, String> {
+    void deleteByUserIds(User user);
 }
