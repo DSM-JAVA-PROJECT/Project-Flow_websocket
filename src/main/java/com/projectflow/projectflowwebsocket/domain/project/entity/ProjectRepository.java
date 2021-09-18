fd48bef9102980e218057cb23a9d24bfbaefa2cc
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-18 22:15:32 +0900

    :bug: :: (#4) Repository ObjectId로 수정

--- src/main/java/com/projectflow/projectflowwebsocket/domain/project/entity/ProjectRepository.java
+++ src/main/java/com/projectflow/projectflowwebsocket/domain/project/entity/ProjectRepository.java
@@ -1,6 +1,7 @@
 package com.projectflow.projectflowwebsocket.domain.project.entity;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProjectRepository extends MongoRepository<Project, String> {
+public interface ProjectRepository extends MongoRepository<Project, ObjectId> {
 }
