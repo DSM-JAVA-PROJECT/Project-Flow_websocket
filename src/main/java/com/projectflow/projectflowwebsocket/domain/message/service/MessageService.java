d8a32b22b76389738519137a3eaecf8eef9cb013
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-21 21:46:24 +0900

    ⚡️ :: (#3) 메세지 로직 추가

--- src/main/java/com/projectflow/projectflowwebsocket/domain/message/service/MessageService.java
+++ src/main/java/com/projectflow/projectflowwebsocket/domain/message/service/MessageService.java
@@ -1,5 +1,7 @@
 package com.projectflow.projectflowwebsocket.domain.message.service;
 
+import com.projectflow.projectflowwebsocket.domain.chat.entity.Chat;
+import com.projectflow.projectflowwebsocket.domain.chat.payload.ChatRequest;
 import com.projectflow.projectflowwebsocket.domain.chatroom.entity.ChatRoom;
 
 public interface MessageService {
@@ -8,4 +10,10 @@ public interface MessageService {
     void sendJoinMessage(String chatRoomId);
 
     void sendResignMessage(String chatRoomId);
+
+    void sendRemoveMessage(String chatRoomId, String chatId);
+
+    void sendMessage(String chatRoomId, Chat chat);
+
+    void sendReadMessage(String chatRoomId);
 }
