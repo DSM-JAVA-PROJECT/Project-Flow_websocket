eebe1f8c4e54427e4d2def9073d90533b7f5c74c
jhhong0509 <jhhong0509@naver.com>
2021-10-05 20:08:16 +0900

    ⚡️ :: (#28) 컨트롤러 까먹음

--- src/main/java/com/projectflow/projectflowwebsocket/domain/project/controller/MemberController.java
+++ src/main/java/com/projectflow/projectflowwebsocket/domain/project/controller/MemberController.java
@@ -1,11 +1,23 @@
 package com.projectflow.projectflowwebsocket.domain.project.controller;
 
+import com.projectflow.projectflowwebsocket.domain.project.payload.ProjectMemberListResponse;
+import com.projectflow.projectflowwebsocket.domain.project.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/{projectId}/chatroom")
 public class MemberController {
 
+    private final MemberService memberService;
+
+    @GetMapping
+    public ProjectMemberListResponse getMemberList(@PathVariable String projectId) {
+        return memberService.getMemberList(projectId);
+    }
 
 }
